db772a7ba1063bc75c52a34123ffbaead5c54d5c
Flávio Faria <fmdfaria.prog@gmail.com>
2025-08-08 12:12:25 -0300

    add ensureAuthorized rota recursos

--- backend/src/infra/http/routes/recursos.routes.ts
+++ backend/src/infra/http/routes/recursos.routes.ts
@@ -5,7 +5,6 @@ import { ensureAuthorized } from '../middlewares/ensureAuthorized';
 const recursosController = new RecursosController();
 
 export async function recursosRoutes(app: FastifyInstance) {
-  app.post('/recursos', recursosController.create);
   app.get('/recursos', recursosController.list);
   app.put('/recursos/:id', recursosController.update);
   app.delete('/recursos/:id', recursosController.delete);
